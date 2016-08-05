05eea8c24407457cebedad8ef298a991414f2455
Alex Rocha <alex.rochas@yahoo.com.br>
2016-08-05 19:06:46 -0300

    Add clone function

--- apt_git/console.py
+++ apt_git/console.py
@@ -1,13 +1,15 @@
 import argparse
 import requests
-
+import subprocess
 
 def parse_args():
     parser = argparse.ArgumentParser(
         description="apt-git is a command line interface for retrieval of projects and information about them from github")
-    subparser = parser.add_subparsers(help="subparser help")
+    subparser = parser.add_subparsers(dest="command", help="commands")
     search_parser = subparser.add_parser('search')
     search_parser.add_argument('pattern', help='pattern for search')
+    clone_parser = subparser.add_parser('clone')
+    clone_parser.add_argument('repo', help='repo to clone in format ":owner/:repo_name"')
     args, unkown = parser.parse_known_args()
     return args
 
@@ -15,12 +17,29 @@ def parse_args():
 def main():
     # https://developer.github.com/v3/search/
     parser = parse_args()
-    pattern = parser.pattern
-    request = requests.get("https://api.github.com/search/repositories?q="+pattern+"+in:name&sort=stars&order=desc")
+    command = parser.command
+    commands[command](parser)
+
+
+def clone(parser):
+    repository = parser.repo
+    request = requests.get("https://api.github.com/repos/" + repository)
+    if request.status_code == 200:
+        clone_url = request.json()['clone_url']
+        subprocess.call(["git","clone", clone_url])
+
+
+def search(pattern):
+    request = requests.get("https://api.github.com/search/repositories?q=" + pattern + "+in:name&sort=stars&order=desc")
     if request.status_code == 200:
         for r in request.json()['items']:
             print(r['name'] + " - " + r['description'])
 
+
+commands = {
+    'search' : search,
+    'clone' : clone
+}
 
 if __name__ == '__main__':
     main()
