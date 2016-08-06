2a36bcce7d0c977dbbc2eaa9f9c90c30a02d08e5
Alex Rocha <alex.rochas@yahoo.com.br>
2016-08-05 21:38:35 -0300

    Add error handling

--- apt_git/console.py
+++ apt_git/console.py
@@ -2,13 +2,14 @@ import argparse
 import requests
 import subprocess
 
+
 def parse_args():
     parser = argparse.ArgumentParser(
         description="apt-git is a command line interface for retrieval of projects and information about them from github")
     subparser = parser.add_subparsers(dest="command", help="commands")
     search_parser = subparser.add_parser('search')
     search_parser.add_argument('pattern', help='pattern for search')
-    clone_parser = subparser.add_parser('clone')
+    clone_parser = subparser.add_parser('install')
     clone_parser.add_argument('repo', help='repo to clone in format ":owner/:repo_name"')
     args, unkown = parser.parse_known_args()
     return args
@@ -21,12 +22,16 @@ def main():
     commands[command](parser)
 
 
-def clone(parser):
+def install(parser):
     repository = parser.repo
     request = requests.get("https://api.github.com/repos/" + repository)
     if request.status_code == 200:
         clone_url = request.json()['clone_url']
-        subprocess.call(["git","clone", clone_url])
+        subprocess.call(["git", "clone", clone_url])
+    elif request.status_code == 404:
+        print("Repository not found.")
+    else:
+        print("An error occuried while trying access remote GitHub API, please try again.")
 
 
 def search(pattern):
@@ -34,11 +39,12 @@ def search(pattern):
     if request.status_code == 200:
         for r in request.json()['items']:
             print(r['name'] + " - " + r['description'])
-
+    else:
+        print("An error occuried while trying access remote GitHub API, please try again.")
 
 commands = {
-    'search' : search,
-    'clone' : clone
+    'search': search,
+    'install': install
 }
 
 if __name__ == '__main__':
